3bf6bfb60df0c6ea3202021ccebbcb72a24971e7
stdiodh <playlistdh@gmail.com>
2026-07-30 16:28:48 +0900

    refactor(cleanup): 미사용 코드와 의존성 정리
    참조되지 않는 타입을 제거하고 JWT 파일명과 테스트 의존성을 정돈합니다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.ninja-squad:springmockk:4.0.2")
 	testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.security:spring-security-test")
 
 	// swaager
